8d4b6372ca692680674dbf6f145ec0d1fbc01eb1
daudi13 <davouma13@gmail.com>
2022-03-21 17:13:44 +0300

    Add content inside the Todocontainer component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "SetUp development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and Add content",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: true
+      },
+    ]
+  }
   render() {
     return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component</p>
-      </div>
+      <ul>
+        {this.state.todos.map(todo => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
     )
   }
 }
