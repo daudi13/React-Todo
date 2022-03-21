901aea17cf6336a0294215d9445f092b34233e19
daudi13 <davouma13@gmail.com>
2022-03-21 18:15:50 +0300

    Remove Li from the TodoList

---
+++ src/components/TodoItems.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TodoItems extends React.Component {
+  render() {
+    return (
+      <li>
+        {this.props.todo.title}
+      </li>
+    )
+  }
+}
+
+export default TodoItems
