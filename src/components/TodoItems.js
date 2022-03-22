2c1406e5be7ebc4c95b961407f3a7a71af8e5bd5
daudi13 <davouma13@gmail.com>
2022-03-22 04:47:32 +0300

    edit the handleChangeProps varibale

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -4,7 +4,7 @@ class TodoItems extends React.Component {
   render() {
     return (
       <li>
-        <input type="checkbox" checked={this.props.todo.completed} onChange={() => console.log('checked')}/>{this.props.todo.title}
+        <input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>{this.props.todo.title}
       </li>
     )
   }
