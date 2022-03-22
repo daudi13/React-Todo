d197a53997a81c6bff6d3ca97eddf3cd5c1e0b29
daudi13 <davouma13@gmail.com>
2022-03-22 11:21:23 +0300

    Add inline styling for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,15 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1>Todo</h1>
+      <h1 style={{
+        fontSize: "6rem",
+        fontWeight: "600",
+        marginBottom: "2rem",
+        lineHeight: "1rem",
+        color: "#ececec",
+        textTransform: "lowercase",
+        textAlign:"center"
+      }}>Todo</h1>
     </header>
   )
 }
