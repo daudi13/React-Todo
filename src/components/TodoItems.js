e2de5fb163b54fd52a6e1d3ca4374fade5b0c123
daudi13 <davouma13@gmail.com>
2022-03-22 11:38:57 +0300

    link  Todoitem.module.css to TodoItems.js

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import styles from './TodoItem.module.css'
 class TodoItems extends React.Component {
   render() {
     return (
-      <li>
-        <input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
+      <li className={styles.item}>
+        <input type="checkbox"className={styles.checkbox} checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
         <button onClick={() => this.props.checkDel(this.props.todo.id)}>Delete</button>
         {this.props.todo.title}
       </li>
