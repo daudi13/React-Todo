23297d9cdc2c91c1c7ce27101ed75c2cbabcf421
daudi13 <davouma13@gmail.com>
2022-03-22 16:52:22 +0300

    refactor InputTodo class component into a function component

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,37 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class InputTodo extends Component {
-  state = {
-    title: ''
-  }
+const InputTodo = (props) =>  {
+  const [title, setTitle] = useState("")
 
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+  const onChange = e => {
+    setTitle(e.target.value)
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title)
-      this.setState({
-        title: ""
-      })
+    if (title.trim()) {
+      this.props.addTodoProps(title)
+      setTitle("")
     } else {
       alert("Please add todo")
     }
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input type="text" className="input-text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange}
-        name="title"/>
-        <button className="input-submit">Submit</button>
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input type="text" className="input-text" placeholder="Add Todo..." value={this.state.title} onChange={onChange}
+      name="title"/>
+      <button className="input-submit">Submit</button>
+    </form>
+  )
 }
 
 export default InputTodo;
